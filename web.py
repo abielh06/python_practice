06c59a2c6fd3ff7172c863cfb66c788c5ca8185f
Abiel Hailemariam <abielh06@gmail.com>
2020-06-22 16:12:47 -0500

    added config file to store credentials

--- web.py
+++ web.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+from flask_mail import Mail, Message
+import config
 
 app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = config.EMAIL,
+    MAIL_PASSWORD = config.PASSWORD,
+)
+mail = Mail(app)
 
 
 @app.route("/")
 def index():
+    msg = Message("Hello", 
+                    sender=config.EMAIL,
+                    body="Testing",
+                    recipients=["hawew41871@vewku.com"] )
+    mail.send(msg)
+
     return 'Hello World!!!!'
 
 # to run the application
